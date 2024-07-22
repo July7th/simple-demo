e4effaab9bfacfd0ce04e55ec7f412dc087b3799
QIQy <luckwolfw@163.com>
2024-07-22 18:38:33 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <n-button @click="showModal = true">
+    来吧
+  </n-button>
+  <n-modal v-model:show="showModal" style="width: 60vw" preset="card" >
+    <NImage width="100" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"/>
+  </n-modal>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { NButton,NModal,NImage } from 'naive-ui'
+
+const showModal = ref(false);
+</script>
